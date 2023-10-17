0d0b0324f2cce1ba6c122878ad0580a2acc028de
Piotr Filipiak <kiddhkane@gmail.com>
2023-10-18 01:28:18 +0200

    Added yargs.

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,20 @@
 #! /usr/bin/env node
 
-console.log("hello_cli");
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+const argv = yargs(hideBin(process.argv))
+  .usage("Usage: $0 <command> [options]")
+  .command("hello", "Greet user")
+  .example("hello -n Elliot", "Greet user Elliot")
+  .alias("n", "name")
+  .nargs("n", 1)
+  .describe("n", "Name of user")
+  .demandOption(["n"])
+  .help("h")
+  .alias("h", "help")
+  .epilog("Created by lilKriT").argv;
+
+console.log("CLI says hello,", argv.name);
+
+// console.log(argv);
